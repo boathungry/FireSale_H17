2222e1ead94fc93d7169cf5d6322a5b6293ce12b
rosaasgeirs <89969874+rosaasgeirs@users.noreply.github.com>
2022-05-04 11:26:41 +0000

    navbar breytt adeins

--- catalog/views.py
+++ catalog/views.py
@@ -5,10 +5,13 @@ from catalog.models import Category
 # Create your views here.
 
 def index(request):
-    context = {'category': Category.objects.all().order_by('name')}
-    return render(request, 'electronics/index.html', context)
+    context = {'categories': Category.objects.all().order_by('name')}
+    return render(request, 'catalog/index.html', context)
+
+
 
 def get_category_by_id(request, id):
     return render(request, 'catalog/catalog_category_details.html', {
         'category': get_object_or_404(Category, pk=id)
     })
+
