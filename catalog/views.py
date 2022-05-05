1d49c0d5357fb9a33a369087f13390b4cf6bdd41
rosaasgeirs <89969874+rosaasgeirs@users.noreply.github.com>
2022-05-05 13:52:37 +0000

    fikt

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from catalog.models import Category, Item
 
-
 # Create your views here.
 from forms.item_form import ItemCreateForm
 
@@ -10,10 +9,9 @@ def index(request):
     if 'category' in request.GET:
         context = {'items': Item.objects.filter(catid__item=request.GET['category'])}
         return render(request, 'catalog/index.html', context)
-    Items = Item.objects.all().order_by('name')
-    context = {'items': Items}
+    items = Item.objects.all().order_by('name')
+    context = {'items': items}
     return render(request, 'catalog/index.html', context)
-
 
 
 def get_item_by_id(request, id):
@@ -38,4 +36,3 @@ def create_item(request):
     return render(request, 'catalog/create_item.html', {
         'form': form
     })
-
