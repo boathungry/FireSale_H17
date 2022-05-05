3f6e131d67bb1a622873fb207332f809d66dfac9
AlbertGunnarsson <albertjaran25@gmail.com>
2022-05-05 13:34:28 +0000

    pages and button for buyout and offer

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # http/localhost:8000/candies
+    # http/localhost:8000/catalog
     path('', views.index, name="catalog-index"),
     path('<int:id>', views.get_item_by_id, name='item_details'),
     path('create_item', views.create_item, name='create_item'),
+    path('buyout_item', views.view_buyout_item, name='buyout_item'),
+    path('offer_item', views.view_offer_item, name='offer_item'),
 
 ]
 
