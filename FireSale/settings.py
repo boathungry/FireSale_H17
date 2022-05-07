362c18dc4373697a1de50799ed2ff5fb5d7151a9
rosaasgeirs <89969874+rosaasgeirs@users.noreply.github.com>
2022-05-07 15:46:59 +0000

    mediadir

--- FireSale/settings.py
+++ FireSale/settings.py
@@ -130,6 +130,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 LOGIN_URL= '/User/login'
 LOGIN_REDIRECT_URL = '/'
 
