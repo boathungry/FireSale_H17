2a9a32ecfb113adc8ac920a691b5b49abe5b8adc
rosaasgeirs <89969874+rosaasgeirs@users.noreply.github.com>
2022-05-11 11:23:08 +0000

    uploaded photos for existing instances

--- forms/item_form.py
+++ forms/item_form.py
@@ -3,14 +3,13 @@ from django import forms
 from catalog.models import Item
 from catalog.models import Category
 
-categories = list(Category.objects.values())
 
 class ItemCreateForm(forms.Form):
     name = forms.CharField(label='Item name', required=True, max_length=255)
     description = forms.CharField(label='Description', required=False, max_length=255)
     condition = forms.CharField(label='Condition', max_length=255)
     buyout = forms.FloatField(label='Buyout Price')
-    catid = forms.ChoiceField(label='Category', choices=categories)
+    catid = forms.ChoiceField(label='Category')
     image = forms.ImageField(label='Photo of item')
 
 
--- static/js/catalog.js
+++ static/js/catalog.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
                 let newHtml = response.data.map(d => {
                     return `<div class="well item">
                                 <a class="single_item" href="/catalog/${d.id}">
-                                    <img class="item-img" src="${d.image}" alt="${d.name}"/>
+                                    <img class="item-img" src="/media/${ d.image.url }" alt="${ d.name }" />
                                     <h4>${d.name}</h4>
                                     <div>${d.description}</div>
                                 </a>
