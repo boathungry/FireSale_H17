90aed0c1c0329edb29a6d55aab11080bcb84c319
AlbertGunnarsson <albertjaran25@gmail.com>
2022-05-12 11:59:12 +0000

    fikt

--- sale/urls.py
+++ sale/urls.py
@@ -4,7 +4,8 @@ urlpatterns = [
     # http/localhost:8000/candies
     path('buyout_item', views.view_buyout_item, name='buyout_item'),
     path('buyout_item/<id>', views.create_checkout, name='create_checkout'),
+    path('billing_checkout/<id>', views.create_billing, name='billing_checkout'),
     path('billing_checkout', views.view_billing, name='billing_checkout'),
-    path('checkout_steps', views.view_checkout_steps, name='checkout_steps')
+    path('checkout_overview/<id>', views.view_checkout_overview, name='checkout_overview')
 
 ]
