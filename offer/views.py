165228c73b432731994768a16659e45bb7b79dc6
rosaasgeirs <89969874+rosaasgeirs@users.noreply.github.com>
2022-05-13 16:16:47 +0000

    no email to non buyers

--- offer/views.py
+++ offer/views.py
@@ -70,33 +70,23 @@ def accept_offer(request, offerid):
     item.offer_accepted = True
     item.save()
     send_email(request, offer, 'Offer accepted')
-    send_email(request, offer, 'Item sold')
     return render(request, 'Offer/offer_accepted.html')
 
 
 def send_email(request, offer, message_type):
     """Send emails once an offer has been made or accepted"""
     message_type_options = {
-        'Offer made': f'Dear {offer.buyerid.name.capitalize()},\n\n'
+        'Offer made': f'Dear {offer.itemid.sellerid.name.capitalize()},\n\n'
                       f'An offer has been made on your item "{offer.itemid.name}" for the amount: {offer.amount:.2f}.\n\n'
                       f'If you wish to accept the offer, log in to your account and accept.\n\n'
                       f'All the best,\n\n'
                       f'FireSale Team',
         'Offer accepted': f'Dear {offer.buyerid.name.capitalize()}\n'
                           f'Your offer on item "{offer.itemid.name}" for the amount {offer.amount:.2f} has been accepted!\n'
-                          f'Please log into your account to finish the checkout process.',
-        'Item sold': f'Unfortunately, the item "{offer.itemid.name}" you made an offer on has had another offer '
-                     f'accepted.\n'
-                     f'Better luck next time!'
+                          f'Please log into your account to finish the checkout process.'
     }
     message_name = message_type
-    if message_name == 'Item sold':
-        buyer_list = Offer.objects.filter(itemid=offer.itemid).exclude(buyerid=offer.buyerid).values_list('buyerid')
-        receiver_email = [buyer.email for buyer in buyer_list]
-        if not receiver_email:
-            return
-    else:
-        receiver_email = [offer.itemid.sellerid.email]
+    receiver_email = [offer.itemid.sellerid.email]
     message = message_type_options[message_type]
     send_mail(
         message_name + f': {offer.itemid.name}',  # subject
@@ -104,4 +94,3 @@ def send_email(request, offer, message_type):
         settings.EMAIL_HOST_USER,  # from email
         receiver_email  # to email
     )
-    return HttpResponse("Email notification sent.")
