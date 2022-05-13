d9c2f489f2810f88e72e4c4a84c9833b4c25d212
rosaasgeirs <89969874+rosaasgeirs@users.noreply.github.com>
2022-05-13 11:51:17 +0000

    flex fix + recent items

--- layout/views.py
+++ layout/views.py
@@ -10,7 +10,7 @@ def index(request):
     authid = request.user.id
     user = User.objects.filter(id=authid)
     user_items = Item.objects.filter(sellerid=authid)
-    items = Item.objects.all().order_by("-id")
+    items = Item.objects.filter(offer_accepted=False).order_by("-id")
     context = {"user": user, "items": items, "user_items": user_items}
     return render(request, 'layout/index.html', context=context)
 
